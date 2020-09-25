b77ed1349c810ef4c182518d0795fde083a62f60
Tarek M. Sayed <tareqmsayed@gmail.com>
2020-09-25 09:21:14 +0200

    [feat]: add basic scripts

---
+++ aptInstall.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+DEPS_LIST=( "$@" )
+DEPS_LIST_SIZE=${DEPS_LIST[0]}
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+RESET=$(tput sgr 0)
+
+for ((i=1 ; i <=$DEPS_LIST_SIZE; i++ ))
+do
+	PACKAGE_CONDITION=$(apt -qq list ${DEPS_LIST[i]})
+	if [[ $PACKAGE_CONDITION == *"installed"* ]]; then
+		echo "${GREEN}$i ${DEPS_LIST[i]} Already Installed$RESET"
+	else
+		echo "${GREEN}$i Installing ${DEPS_LIST[i]}$RESET"
+		sudo apt-get install --assume-yes "${DEPS_LIST[i]}"
+		while [[ $(apt -qq --installed list ${DEPS_LIST[i]}) != *"installed"* ]]; do
+			if [[ ${DEPS_LIST[i]} == *"="* ]]; then
+				if [[ $(apt -qq --installed list $(echo ${DEPS_LIST[i]} | grep -Po '.+(?==)')) == *"installed"* ]]; then
+					echo "${GREEN}$i ${DEPS_LIST[i]} Installed$RESET"
+					break
+				fi
+			fi
+			echo "${RED}$i Retry Installing ${DEPS_LIST[i]} after 20 Seconds$RESET"
+			# use escape password to escape installing failed packages
+			read -rs -t 20 -p "${GREEN}Enter pass to Escape installing Package or Press Enter to Retry Now: $RESET" ESC
+            if [[ $ESC == "pass" ]]; then
+                echo -e "\n${RED}Escape package ${DEPS_LIST[i]}$RESET"
+                break
+            else
+                echo -e "\n"
+            fi
+			sudo apt-get install --assume-yes "${DEPS_LIST[i]}"
+		done
+	fi
+done
---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+INSTALL_SRC_DIR=$(pwd)
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+RESET=$(tput sgr 0)
+
+echo "$GREEN Installing Basic Packages $RESET"
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y
+APPS=(
+	python3-dev python3-pip 
+	synaptic ubuntu-restricted-extras gufw 
+	p7zip-rar p7zip-full unace unrar zip unzip sharutils rar uudeview mpack arj cabextract
+	file-roller ssh git curl gnome-tweaks dconf-editor)
+$INSTALL_SRC_DIR/aptInstall.sh "${#APPS[@]}" "${APPS[@]}"
+
+
+APPS=()
+
+unset answer
+APP='qbittorrent'
+read -p "$YELLOW do you want to install $APP [y/N]: $RESET" answer
+if [[ $answer =~ y|Y|yes ]]; then
+	echo "$GREEN Adding $APP $RESET"
+	sudo add-apt-repository ppa:qbittorrent-team/qbittorrent-stable
+	APPS+=(qbittorrent)
+else
+	echo "$RED $APP will not installed $RESET"
+fi
+
+
+echo "$GREEN Installing chosen packages Packages $RESET"
+$INSTALL_SRC_DIR/aptInstall.sh "${#APPS[@]}" "${APPS[@]}"
