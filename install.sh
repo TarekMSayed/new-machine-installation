f3291532ee022e1627dae32fa29efb324e920fa8
Tarek M. Sayed <tareqmsayed@gmail.com>
2020-09-26 10:38:35 +0200

    [fix]: assume yes when adding repositories

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ RESET=$(tput sgr 0)
 echo "$GREEN Installing Basic Packages $RESET"
 # enable Canonical Partners repo
 sudo sed -i "s~# deb http~deb http~g" /etc/apt/sources.list
-sudo add-apt-repository ppa:libreoffice/ppa
+sudo add-apt-repository -y ppa:libreoffice/ppa
 sudo apt update
 sudo apt upgrade -y
 APPS=(
@@ -34,7 +34,7 @@ APP='qbittorrent'
 read -p "$YELLOW Do you want to install $APP [y/N]: $RESET" answer
 if [[ $answer =~ y|Y|yes ]]; then
   echo "$GREEN Adding $APP $RESET"
-  sudo add-apt-repository ppa:qbittorrent-team/qbittorrent-stable
+  sudo add-apt-repository -y ppa:qbittorrent-team/qbittorrent-stable
   APPS+=(qbittorrent)
 else
   echo "$RED $APP will not installed $RESET"
@@ -45,7 +45,7 @@ APP='MKVtoolnix'
 read -p "$YELLOW Do you want to install $APP [y/N]: $RESET" answer
 if [[ $answer =~ y|Y|yes ]]; then
   echo "$GREEN Adding $APP $RESET"
-  sudo add-apt-repository "deb https://mkvtoolnix.download/ubuntu/ $(lsb_release -sc) main"
+  sudo add-apt-repository -y "deb https://mkvtoolnix.download/ubuntu/ $(lsb_release -sc) main"
   wget -q -O - https://mkvtoolnix.download/gpg-pub-moritzbunkus.txt | sudo apt-key add -
   APPS+=(mkvtoolnix-gui)
 else
@@ -57,7 +57,7 @@ APP='CodeBlocks'
 read -p "$YELLOW Do you want to install $APP [y/N]: $RESET" answer
 if [[ $answer =~ y|Y|yes ]]; then
   echo "$GREEN Adding $APP $RESET"
-  sudo add-apt-repository ppa:damien-moore/codeblocks-stable
+  sudo add-apt-repository -y ppa:damien-moore/codeblocks-stable
   APPS+=(codeblocks)
 else
   echo "$RED $APP will not installed $RESET"
@@ -68,7 +68,7 @@ APP='telegram'
 read -p "$YELLOW Do you want to install $APP [y/N]: $RESET" answer
 if [[ $answer =~ y|Y|yes ]]; then
   echo "$GREEN Adding $APP $RESET"
-  sudo add-apt-repository ppa:atareao/telegram
+  sudo add-apt-repository -y ppa:atareao/telegram
   APPS+=(telegram)
 else
   echo "$RED $APP will not installed $RESET"
@@ -79,7 +79,7 @@ APP='VScode'
 read -p "$YELLOW Do you want to install $APP [y/N]: $RESET" answer
 if [[ $answer =~ y|Y|yes ]]; then
   echo "$GREEN Adding $APP $RESET"
-  sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+  sudo add-apt-repository -y "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
   wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
   APPS+=(code)
 else
@@ -91,7 +91,7 @@ APP='sublime-text-3'
 read -p "$YELLOW Do you want to install $APP [y/N]: $RESET" answer
 if [[ $answer =~ y|Y|yes ]]; then
   echo "$GREEN Adding $APP $RESET"
-  sudo add-apt-repository "deb https://download.sublimetext.com/ apt/stable/"
+  sudo add-apt-repository -y "deb https://download.sublimetext.com/ apt/stable/"
   wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
   APPS+=(sublime-text)
 else
@@ -103,7 +103,7 @@ APP='docker-ce'
 read -p "$YELLOW Do you want to install $APP [y/N]: $RESET" answer
 if [[ $answer =~ y|Y|yes ]]; then
   echo "$GREEN Adding $APP $RESET"
-  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+  sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
   sudo groupadd docker
   sudo usermod -aG docker ${USER}
