a864ec87408b74d25db3736c32a82fe7ccfbd75a
Tarek M. Sayed <tareqmsayed@gmail.com>
2020-09-26 09:41:43 +0200

    [feat]: add mkvtoolnix installation

--- install.sh
+++ install.sh
@@ -10,8 +10,8 @@ echo "$GREEN Installing Basic Packages $RESET"
 sudo apt update
 sudo apt upgrade -y
 APPS=(
-  python3-dev python3-pip 
-  synaptic ubuntu-restricted-extras gufw 
+  python3-dev python3-pip virtualenv
+  synaptic ubuntu-restricted-extras gufw apt-transport-https
   p7zip-rar p7zip-full unace unrar zip unzip sharutils rar uudeview mpack arj cabextract
   file-roller ssh git curl gnome-tweaks dconf-editor)
 $INSTALL_SRC_DIR/aptInstall.sh "${#APPS[@]}" "${APPS[@]}"
@@ -36,6 +36,17 @@ else
   echo "$RED $APP will not installed $RESET"
 fi
 
+unset answer
+APP='MKVtoolnix'
+read -p "$YELLOW Do you want to install $APP [y/N]: $RESET" answer
+if [[ $answer =~ y|Y|yes ]]; then
+  echo "$GREEN Adding $APP $RESET"
+  sudo add-apt-repository "deb https://mkvtoolnix.download/ubuntu/ $(lsb_release -sc) main"
+  wget -q -O - https://mkvtoolnix.download/gpg-pub-moritzbunkus.txt | sudo apt-key add -
+  APPS+=(mkvtoolnix-gui)
+else
+  echo "$RED $APP will not installed $RESET"
+fi
 
 echo "$GREEN Installing chosen packages $RESET"
 sudo apt update
