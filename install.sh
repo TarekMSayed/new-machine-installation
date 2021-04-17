3b3bef59e823e1d5954dc75beede3451aac47b5b
Tarek M. Sayed <tareqmsayed@gmail.com>
2021-04-17 17:40:50 +0200

    [fix]: add vscode repo

--- install.sh
+++ install.sh
@@ -93,8 +93,8 @@ APP='VScode'
 read -p "$YELLOW Do you want to install $APP [y/N]: $RESET" answer
 if [[ $answer =~ y|Y|yes ]]; then
   echo "$GREEN Adding $APP $RESET"
-#  sudo add-apt-repository -y "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-#  wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+ wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+ sudo add-apt-repository -y "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
   APPS+=(code)
 else
   echo "$RED $APP will not installed $RESET"
